1dd4c76f127b2948ffc1ca27c2ef93fd8f5af693
IvanIglesias7 <csi21-iiglhez@colegioaltair.net>
2023-09-21 12:22:59 +0200

    First commit

---
+++ Repaso1/Modelo/empleado.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso1.Modelo
+{
+    public class empleado
+    {
+        string nombre;
+        string apellidos;
+        string dni;
+        DateTime fechaNac;
+        int NSeguridadS;
+        int NCuenta;
+
+        public empleado(string nombre, string apellidos, string dni, DateTime fechaNac, int nSeguridadS, int nCuenta)
+        {
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.dni = dni;
+            this.fechaNac = fechaNac;
+            NSeguridadS = nSeguridadS;
+            NCuenta = nCuenta;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellidos { get => apellidos; set => apellidos = value; }
+        public string Dni { get => dni; set => dni = value; }
+        public DateTime FechaNac { get => fechaNac; set => fechaNac = value; }
+        public int NSeguridadS1 { get => NSeguridadS; set => NSeguridadS = value; }
+        public int NCuenta1 { get => NCuenta; set => NCuenta = value; }
+    }
+}
---
+++ Repaso1/Tools/menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso1.Tools
+{
+    public class tools
+    {
+
+        public void menu()
+        {
+            Console.WriteLine("Bienvenid@ a la app!");
+
+            Console.WriteLine("1. Añadir un empleado");
+            Console.WriteLine("2. Modificar un empleado");
+            Console.WriteLine("3. Exportar a fichero");
+            Console.WriteLine("0. Salir");
+        }
+
+        public int opcionMenu()
+        {
+            Console.WriteLine("Elige una opcion: ");
+            int num = int.Parse(Console.ReadLine());
+
+            return num;
+        }
+    }
+}
